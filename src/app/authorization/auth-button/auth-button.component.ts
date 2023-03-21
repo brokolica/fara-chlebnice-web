bd1cf4a535509b5d958a0a71cbb31f8006c455a8
Adam Bacho <Adam.Bacho@newsandmedia.sk>
2023-03-21 19:53:43 +0100

    Added authorization.

---
+++ src/app/authorization/auth-button/auth-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styles: [],
+})
+export class AuthButtonComponent {
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+
+  public loginWithRedirect() {
+    this.auth.loginWithRedirect({
+      authorizationParams: {
+        audience: "https://fara-chlebnice-lc3g8.ondigitalocean.app/api/"
+      }
+    });
+  }
+}
